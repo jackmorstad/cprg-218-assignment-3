c8ea851e57c3325cb2f9858500f538c29d9745b7
jackmorstad <jackmorstad@gmail.com>
2025-04-16 14:36:19 -0600

    final changes

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// Your OpenWeatherMap API key
-const apiKey = '095ced10b64a8a9916ed2be3418e63c9'; // Replace with your own API key
+
+const apiKey = '095ced10b64a8a9916ed2be3418e63c9';  
 const city = 'Cancun';
 const country = 'MX';
 const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=metric&appid=${apiKey}`;
 
-// Fetch the weather data
+
 fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
@@ -12,7 +12,6 @@ fetch(apiUrl)
         const weatherDescription = data.weather[0].description;
         const weatherElement = document.getElementById('weather');
         
-        // Update the weather section with the fetched data
         weatherElement.innerHTML = `Weather: ${temperature}°C, ${weatherDescription.charAt(0).toUpperCase() + weatherDescription.slice(1)} (Cancun, Mexico)`;
     })
     .catch(error => {
